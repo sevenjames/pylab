8ed2dbcc7ddcd54d4644ef4cc73f364b77e29d27
sevenjames <sevenjames@users.noreply.github.com>
2020-05-22 00:23:40 -0500

    Update p200521_bincop.py
    parse command line args function

--- p200521_bincop.py
+++ p200521_bincop.py
@@ -8,5 +8,31 @@ bincop inputfile outputfile
     if outputfile does exist, fail, print error
     load inputfile (in chunks?)
     write outputfile
+
+consider
+    sys.argv or argparse
+    os.path.exists('./file')
+    with open(mode='rb')
+
 '''
 
+import sys
+import os.path
+
+def parse():
+    try:
+        if not len(sys.argv) == 3:
+            raise ValueError('Usage: bincop inputfile outputfile')
+        if not os.path.exists(sys.argv[1]):
+            raise ValueError('Input file does not exist: ', sys.argv[1])
+        if os.path.exists(sys.argv[2]):
+            raise ValueError('Output file already exists: ', sys.argv[2])
+    except ValueError as err:
+        print(''.join(err.args))
+    else:
+        process()
+
+def process():
+    print('Ok')
+
+parse()
