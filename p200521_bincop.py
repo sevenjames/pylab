3d98181f530f0fb85a99033b1e71ee39dbda288e
sevenjames <sevenjames@users.noreply.github.com>
2020-05-21 22:17:04 -0500

    Create p200521_bincop.py

---
+++ p200521_bincop.py
@@ -0,0 +1,12 @@
+'''
+2020-05-21
+binary copy a file
+
+bincop inputfile outputfile
+    if args count is not 2, fail, print usage
+    if inputfile doesn't exist, fail, print error
+    if outputfile does exist, fail, print error
+    load inputfile (in chunks?)
+    write outputfile
+'''
+
