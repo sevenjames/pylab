8be0d1970a01cad59223e8c39bcdd66d8ef43ca6
sevenjames <sevenjames@users.noreply.github.com>
2020-06-14 15:57:05 -0500

    Update meowworld.py
    variables enhance

--- meowworld.py
+++ meowworld.py
@@ -1,8 +1,10 @@
-def meow(s):
-    u = ''.join([chr(i) for i in range(65,91)])
-    l = ''.join([chr(i) for i in range(97,123)])
-    a = ''.join([''.join(i) for i in list(zip(u,l))])
-    return ''.join([a[(a.find(c)+26)%52] if c in a else c for c in s])
+def meow(snack):
+    (c,a,t) = (65,26,32)
+    (m,e,o,w) = (c,c+a,c+t,c+a+t)
+    purr = ''.join([chr(i) for i in range(m,e)])
+    hiss = ''.join([chr(i) for i in range(o,w)])
+    chew = ''.join([''.join(i) for i in list(zip(purr,hiss))])
+    return ''.join([chew[(chew.find(bite)+a)%(a+a)] if bite in chew else bite for bite in snack])
 
 def give(s):
     return bytes.decode(bytes.fromhex(s))
