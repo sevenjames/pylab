06f007367332fd59f8f13249bf0f3041da210e8e
sevenjames <sevenjames@users.noreply.github.com>
2020-06-15 02:59:27 -0500

    Update bincop.py
    rewrite

--- bincop.py
+++ bincop.py
@@ -5,47 +5,35 @@ usage: bincop inputfile outputfile
 """
 
 import sys
-import os.path
-
-
-def file_read(file):
-    try:
-        with open(file, mode='rb') as fob:
-            return fob.read()
-    except OSError as error:
-        print(f'Read error: {error}')
-        raise
-
-def file_write(file, data):
-    try:
-        with open(file, mode='wb') as fob:
-            fob.write(data)
-    except OSError as error:
-        print(f'Write error: {error}')
-        raise
-
-def check_args(args):
-    try:
-        if len(args) != 3:
-            raise ValueError('Wrong number of arguments.')
-        if not os.path.exists(args[1]):
-            raise ValueError('Input file not found.')
-        if os.path.exists(args[2]):
-            raise ValueError('Output file already exists.')
-    except ValueError as error:
-        print(f'Error: {error}')
-        print('Usage: bincop inputfile outputfile')
-    else:
-        return True
 
 def main():
-    """load entire file into bytes object, then write that object out"""
-    if check_args(sys.argv):
-        infile, outfile = sys.argv[1:3]
-        print(f'Copying {infile} to {outfile}')
-        data = file_read(infile)
-        file_write(outfile, data)
-        print('Copy success.')
+    """load entire file into bytes object, then write that object out.
+    open mode rb : read bytes, fails if file not found
+    open mode xb : write bytes, exclusive create, fail if file is found
+    """
+    try:
+        if len(sys.argv) != 3:
+            raise ValueError('Wrong number of arguments.')
+    except ValueError as error:
+        print(f'Error: {error}')
+        print('Usage: bincop.py <inputfile> <outputfile>')
+    else:
+        print(f'Copying {sys.argv[1]} to {sys.argv[2]}')
+        try:
+            with open(sys.argv[1], mode='rb') as fob:
+                data = fob.read()
+        except OSError as error:
+            print(f'File error: {error}')
+        else:
+            # process(data) goes here
+            try:
+                with open(sys.argv[2], mode='xb') as fob:
+                    fob.write(data)
+            except OSError as error:
+                print(f'File error: {error}')
+            else:
+                print('ok')
 
 if __name__ == "__main__":
     main()
+    # args_thing()
