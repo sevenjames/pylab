6960d618a2c5c1764b1fe2533b1f1df13f6a46bb
sevenjames <sevenjames@users.noreply.github.com>
2020-06-14 23:20:21 -0500

    Update meowworld.py
    remove unnecessary square brackets

--- meowworld.py
+++ meowworld.py
@@ -1,13 +1,14 @@
 def meow(snack):
     (c, a, t) = (65, 26, 32)
     (m, e, o, w) = (c, c+a, c+t, c+a+t)
-    purr = ''.join([chr(i) for i in range(m,e)])
-    hiss = ''.join([chr(i) for i in range(o,w)])
-    chew = ''.join([''.join(i) for i in list(zip(purr,hiss))])
-    return ''.join([chew[(chew.find(bite)+a)%(a+a)] if bite in chew else bite for bite in snack])
+    purr = ''.join(chr(i) for i in range(m, e))
+    hiss = ''.join(chr(i) for i in range(o, w))
+    chew = ''.join(''.join(i) for i in list(zip(purr, hiss)))
+    return ''.join(chew[(chew.find(bite)+a)%(a+a)] if bite in chew else bite for bite in snack)
 
 def give(s):
     return bytes.decode(bytes.fromhex(s))
 
 tuna = '5572797962206a626579712e'
+
 print(meow(give(tuna)))
