07602f1dd35a5fc2b9d183085e7442a07b94b79e
sevenjames <sevenjames@users.noreply.github.com>
2020-06-15 21:07:11 -0500

    Update meowworld.py
    add new var for repeated value.
    rename and rearrange funcs.
    test for main and call sample func.

--- meowworld.py
+++ meowworld.py
@@ -1,21 +1,23 @@
-def meow_one(snack):
+def meow(snack):
+    (c, a, t, s) = (65, 26, 32, 13)
+    purr = ''.join(chr(i) for i in range(c, c+a))
+    chew = ''.join(chr(i) for i in range(c+t, c+a+t))
+    eat = dict(zip(purr+chew, purr[s:]+purr[:s]+chew[s:]+chew[:s]))
+    return ''.join([eat.get(bite, bite) for bite in snack])
+
+def meowmeow(snack):
     (c, a, t) = (65, 26, 32)
     purr = ''.join(chr(i) for i in range(c, c+a))
     chew = ''.join(chr(i) for i in range(c+t, c+a+t))
     eat = ''.join(''.join(i) for i in list(zip(purr, chew)))
     return ''.join(eat[(eat.find(bite)+a)%(a+a)] if bite in eat else bite for bite in snack)
 
-def meow_two(snack):
-    (c, a, t) = (65, 26, 32)
-    purr = ''.join(chr(i) for i in range(c, c+a))
-    chew = ''.join(chr(i) for i in range(c+t, c+a+t))
-    eat = dict(zip(purr+chew, purr[13:]+purr[:13]+chew[13:]+chew[:13]))
-    return ''.join([eat.get(bite, bite) for bite in snack])
-
 def give(s):
     return bytes.decode(bytes.fromhex(s))
 
-tuna = '5572797962206a626579712e'
+def sample():
+    tuna = '5572797962206a626579712e'
+    print(meow(give(tuna)))
 
-print(meow_one(give(tuna)))
-print(meow_two(give(tuna)))
+if __name__ == "__main__":
+    sample()
