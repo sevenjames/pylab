0bdecd806092ad5d7073cdb145aca552335c5ef2
sevenjames <sevenjames@users.noreply.github.com>
2020-06-15 19:42:20 -0500

    Update meowworld.py
    eliminate variable variations between the two versions

--- meowworld.py
+++ meowworld.py
@@ -1,10 +1,9 @@
 def meow_one(snack):
     (c, a, t) = (65, 26, 32)
-    (m, e, o, w) = (c, c+a, c+t, c+a+t)
-    purr = ''.join(chr(i) for i in range(m, e))
-    hiss = ''.join(chr(i) for i in range(o, w))
-    chew = ''.join(''.join(i) for i in list(zip(purr, hiss)))
-    return ''.join(chew[(chew.find(bite)+a)%(a+a)] if bite in chew else bite for bite in snack)
+    purr = ''.join(chr(i) for i in range(c, c+a))
+    chew = ''.join(chr(i) for i in range(c+t, c+a+t))
+    eat = ''.join(''.join(i) for i in list(zip(purr, chew)))
+    return ''.join(eat[(eat.find(bite)+a)%(a+a)] if bite in eat else bite for bite in snack)
 
 def meow_two(snack):
     (c, a, t) = (65, 26, 32)
